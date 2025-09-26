38bc25470909e633ccb4462545b47d2517c6adfa
t3r6 <test>
2025-09-27 01:01:44 +0300

    Align decompiled Specmode.lua

--- HUD/Specmode.lua
+++ HUD/Specmode.lua
@@ -668,7 +668,7 @@ function PSpectatorControler:ProHud(he, ar, am1, am2, cwi, art)
   local alcolor = { {255, 255, 255}, {0, 102, 255}, {255, 0, 0}, {255, 255, 0}, {255, 255, 153}, {0, 0, 0}, {255, 0, 153} }
 
   local i = cwi
-  if type(i) == "number" and i <= 7 and 1 <= i and cwi == i then
+  if type(i) == "number" and i <= 7 and i >= 1 and cwi == i then
     Hud:QuadRGBA(cupriammolist[i], (1024 - 57 * Cfg.HUDSize) * w / 1024, (768 + Cfg.HUDSize * 13 - Cfg.HUDSize * sizey) * h / 768, Cfg.HUDSize, false, prcolor[i][1], prcolor[i][2], prcolor[i][3], 255)
     Hud:QuadRGBA(cualtammolist[i], (1024 - 57 * Cfg.HUDSize) * w / 1024, (768 + Cfg.HUDSize * 48 - Cfg.HUDSize * sizey) * h / 768, Cfg.HUDSize, false, alcolor[i][1], alcolor[i][2], alcolor[i][3], 255)
     Hud:DrawDigitsText1((1024 - 117 * Cfg.HUDSize) * w / 1024, (768 + Cfg.HUDSize * 15 - Cfg.HUDSize * sizey) * h / 768, string.sub(string.format(cunumcharact[i], am1), -3), showinf * Cfg.HUDSize, cupriammolistnumwar[i])
