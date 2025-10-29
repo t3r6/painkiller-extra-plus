5ced4d9de7e828f6a06235d45eab3b05124e7129
fghvgsx <atloidcute@outlook.com>
2025-10-29 04:05:05 +0000

    fix offset

--- HUD/HUD.lua
+++ HUD/HUD.lua
@@ -1817,7 +1817,7 @@ function Hud:DrawItemTimers()
 
             local size = (Cfg.HUD_Spec_Item_Timers_Size or 0) > 0 and Cfg.HUD_Spec_Item_Timers_Size or 35
             local mw, mh = size, size -- MATERIAL.Size(mat)
-            local dw = w - (posX*w/1024 + mw*2)
+            local dw = (1024-posX-mw*1.6) * w/1024
 
             local r,g,b = unpack(Hud.TimerMats[o._type][2])
             if side then
@@ -1825,6 +1825,7 @@ function Hud:DrawItemTimers()
                 dw = posX*w/1024 + mw
             else
                 HUD.DrawQuadRGBA(mat,w-(posX*w/1024 + mw),(posY+offset-mh*1/15)*h/768,mw,mh,r,g,b)
+                if o._timeleft > 99 then dw = (1024-posX-mw*2) * w/1024 end
             end
 
             r, g, b = 255, 255, 255
