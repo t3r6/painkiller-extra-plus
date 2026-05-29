934d3f642362a559423e17263e576dfce356feb5
t3r6 <test>
2026-05-30 01:51:53 +0300

    fix

--- Main/GameMP.lua
+++ Main/GameMP.lua
@@ -1344,7 +1344,7 @@ function Game:PlayerRespawnRequest(clientID)
         end
         Game.PlayerRespawnConfirmation(clientID,player._Entity,ENTITY.GetOrientation(player._Entity),weapon)
         if MPCfg.GameMode == "People Can Fly" and Cfg.PCFWeapons then -- required for compatibility with old clients
-            for i,o in {"IShotgunFZ","IStakeGunGL","IMiniGunRL","IDriverElectro","IRifleFlameThrower","IBoltGunHeater"} do
+            for i,o in {"IShotgunFZ","IStakeGunGL","IDriverElectro","IRifleFlameThrower","IBoltGunHeater","IMiniGunRL"} do -- MiniGunRL takes precedence if last
                 Templates[o..".CItem"].TakeFX(player._Entity,999,999)
             end
         end
